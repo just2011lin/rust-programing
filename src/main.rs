35bc93bed529c5fbc72f6b1a05f55828f025b4c1
just2011lin <chi_dou@qq.com>
2024-07-20 10:27:16 +0800

    1

--- src/main.rs
+++ src/main.rs
@@ -6,4 +6,5 @@ fn main() {
     mod_parent::child::print("this is main");
     mod_other::other_print();
     mod_parent::print_parent();
+    println!("CHILD is {}", mod_parent::child::CHILD);
 }
--- src/mod_parent/child.rs
+++ src/mod_parent/child.rs
@@ -1,5 +1,7 @@
 // use crate::mod_other;
 
+pub const CHILD: i32 = 20;
+
 pub fn print(s: &str) {
     println!("{s}");
     // mod_other::other_print();
