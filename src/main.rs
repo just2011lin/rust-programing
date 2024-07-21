f9ff10eaaabe18a63284c17485abcc66c5027e6c
just2011lin <chi_dou@qq.com>
2024-07-22 07:40:32 +0800

    clear

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,3 @@
-use rust_programing::Post;
 fn main() {
-    let mut post = Post::new();
-    post.add_text("今天中午我吃了鸡蛋");
-
-    let post = post.request_review();
-    let post = post.approve();
-    println!("content: {}", post.content());
+    
 }
